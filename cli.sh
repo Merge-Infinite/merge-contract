4abc0915cdb5be6f018da8ca77f370ccbab0e408
Tom Cat <48447545+tx-tomcat@users.noreply.github.com>
2025-06-10 14:38:48 +0700

    update reward pools

--- cli.sh
+++ cli.sh
@@ -5,12 +5,12 @@ sui client call \
   --args \
     0x74a21ae4b05105d003d35b51faea9905abed2668074b75862156fbf6349fcb74 \
    0xcd698cd0dba0f42fbbddf94cb3c578a7512aaf96d3d151264ab2879f120dd2ff \
-    "Pool Name" \
-    "Pool Description" \
-    '[]' \
-    1749207278815 \
-    "https://app.merg3.xyz/images/sui.svg" \
+    "Pool Name 4" \
+    "Pool Description 4" \
+    '[491271,21,13]' \
     1749379144000 \
+    "https://app.merg3.xyz/images/sui.svg" \
+    1750074557000 \
     0x6 \
   --gas-budget 10000000
 
@@ -21,7 +21,7 @@ sui client call \
   --args \
     0x74a21ae4b05105d003d35b51faea9905abed2668074b75862156fbf6349fcb74 \
    0xcd698cd0dba0f42fbbddf94cb3c578a7512aaf96d3d151264ab2879f120dd2ff \
-    0x89d1a3d196ce1c0deaa07acf49fb3aa9744c186d934d46eea64efac82ffeb90a \
+    0x39ae497da9c3969b9d3d77602a554e27be0ef48f1fcaf2290dfacb84bf057c8f \
     0x6 \
   --gas-budget 10000000
 
@@ -32,7 +32,7 @@ sui client call \
   --args \
     0x74a21ae4b05105d003d35b51faea9905abed2668074b75862156fbf6349fcb74 \
    0xcd698cd0dba0f42fbbddf94cb3c578a7512aaf96d3d151264ab2879f120dd2ff \
-    0x8c7c6e34b85668412754694f852b83d709b8dcd9e7d37eb9e296e2968db8fb0b \
+    0x89d1a3d196ce1c0deaa07acf49fb3aa9744c186d934d46eea64efac82ffeb90a \
     0x6 \
   --gas-budget 10000000
 
